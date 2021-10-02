59314701dad10d5ac005ed9b426a4360fdc236c8
evanhosni <evankhaledhosni@gmail.com>
2021-10-01 18:37:33 -0700

    basic html structure and beginning of js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('sup')
+var city
+var APIKey = 'fed67933791ff7a3c16fb39c61d918b3'
+var weatherData
+var now = moment().format('M/DD/YYYY');
+
+$('#search-button').click(function() {
+    city = $('#search-field').val().trim()
+    weatherData = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIKey
+    fetch(weatherData)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    
+})
